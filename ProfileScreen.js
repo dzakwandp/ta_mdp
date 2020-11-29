9273bcbd40c3ae17410b39937bee74db2c654f9d
bagusaff <fkrfajri@gmail.com>
2020-11-29 20:22:22 +0700

    added 2 bottom navigator , added splash screen with premier league badge

---
+++ ProfileScreen.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+
+export default function ProfileScreen({navigation}) {
+    return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+    </View>
+    )
+}
